b5ca4935003afdfd5ca79c16aff19a8b9de9aa59
ganeshkadu1 <ganeshkadu20@gmail.com>
2022-02-12 18:00:47 +0530

    Added Animation to drum

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ for (var i = 0; i < numberOfDrum; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 //Detecting Keyboard Press
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -54,4 +56,11 @@ function makeSound(key) {
       console.log(buttonInnerHTML);
       break;
   }
+}
+
+// Added Animation to drum
+function buttonAnimation(currentKey){
+   var activeButton = document.querySelector("." + currentKey);
+   activeButton.classList.add("pressed");
+   setTimeout(function(){activeButton.classList.remove("pressed")},100);
 }
